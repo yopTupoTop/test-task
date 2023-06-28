78e34234f65eab18fa57bf90fa27ba448ce9efeb
Tatiana <user@Air-User.lan>
2023-06-28 13:25:53 +0400

    init

---
+++ contracts/NftCollection.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract NftCollection is ERC721, ERC721Enumerable {
+    event CollectionCreated(address collectionAddress, string name, string symbol);
+    event TokenMinted(address collectionAddress, address recipient, uint256 tokenId, string tokenUri);
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+
+    }
+
+     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
